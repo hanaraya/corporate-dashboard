3c93460a5f1951a7bb224720dce8b2d238067516
hanaraya <n.harish22@gmail.com>
2016-05-26 23:49:45 -0700

    feat: added table for displaying issues

--- app/routes/issues.js
+++ app/routes/issues.js
@@ -1,4 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	model: function(){
+		return Ember.$.get('issue/issues.csv').then(function(response){
+			var allRecords = response.split(/\r\n|\n/);
+			var table = {};
+			var rows = [];
+			allRecords.forEach(function(record, index){
+				var columns = record.split(/,/);
+				if(index === 0)
+					table.header = columns;
+				else
+					rows.push(columns);
+			});
+			table.rows = rows;
+			console.log(table);
+			return table;
+		});
+
+
+	}
 });
